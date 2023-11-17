3289e054f84eaf588dc29a92b9f1e5ed157ac78a
Сергей Винчестер <creepmah@gmail.com>
2023-11-17 16:58:01 +0300

    N523

---
+++ LeetCodeProblems/N523_Continuous_Subarray_Sum.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeProblems
+{
+    public class N523_Continuous_Subarray_Sum
+    {
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+            {
+                return false;
+            }
+
+            int[] sums = new int[nums.Length];
+            sums[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sums[i] = sums[i - 1] + nums[i];
+            }
+
+            int[] rests = new int[sums.Length];
+            for (int i = 0; i < sums.Length; i++)
+            {
+                int rest = sums[i] % k;
+                if (rests.Contains(rest))
+                {
+                    return true;
+                }
+                else
+                {
+                    rests[i] = rest;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            N523_Continuous_Subarray_Sum n523_Continuous_Subarray_Sum = new N523_Continuous_Subarray_Sum();
+            n523_Continuous_Subarray_Sum.CheckSubarraySum(new int[] { 23, 2, 6, 4, 7 }, 13);
         }
     }
 }
